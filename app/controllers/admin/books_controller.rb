3779813d5371abad67c13500231ae726cb315b83
Steve Thomas <gamgi1@hotmail.com>
2015-06-09 14:23:43 -0600

    fill book controller

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,24 +1,49 @@
 class Admin::BooksController < ApplicationController
     layout 'admin'
+    before_action :find_book, only: [:show, :edit, :update, :destroy]
 
     def index
+      @books = Books.all
     end
 
     def new
+      @book = Book.new
     end
 
     def create
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to admin_books_path
+      else
+        render :new
+      end
     end
 
     def edit
     end
 
     def update
+      if @book.update(book_params)
+        redirect_to admin_books_path
+      else
+        render :edit
+      end
     end
 
     def show
     end
 
     def destroy
+      @book.destroy
+      redirect_to admin_books_path
+    end
+
+    private
+    def find_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author, :pages, :price)
     end
 end
