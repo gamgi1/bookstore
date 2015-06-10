8a64e474a40e1d110203e266225a47117933f9d6
Steve Thomas <gamgi1@hotmail.com>
2015-06-10 15:23:10 -0600

    rows highlight red when out of stock

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def administrator_signed_in?
     current_administrator.present?
   end
+  def danger_none_left(stock)
+    if stock <= 0
+      return "danger"
+    end
+  end
 end
